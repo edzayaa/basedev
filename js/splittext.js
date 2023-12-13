a57d2d9cef2f714f7c73f9b9c309cca0bb75165c
edzayaa <138071948+edzayaa@users.noreply.github.com>
2023-12-13 17:53:47 -0400

    update
    update

--- js/splittext.js
+++ js/splittext.js
@@ -1,32 +1,34 @@
-const quotes = document.querySelectorAll(".quote");
+// Effect1 
+const splittext1 = document.querySelectorAll(".splittext1");
 
-function setupSplits() {
-  quotes.forEach(quote => {
-    // Reset if needed
-    if(quote.anim) {
-      quote.anim.progress(1).kill();
-      quote.split.revert();
-    }
+function splitText1() {
+    splittext1.forEach(quote => {
+        // Reset if needed
+        if(quote.anim) {
+            quote.anim.progress(1).kill();
+            quote.split.revert();
+        }
 
-    quote.split = new SplitText(quote, { 
-      type: "lines,words,chars",
-      linesClass: "split-line"
+        quote.split = new SplitText(quote, { 
+            type: "lines,words,chars",
+            linesClass: "split-line"
+        });
+
+        // Set up the anim
+        quote.anim = gsap.from(quote.split.chars, {
+            scrollTrigger: {
+                trigger: quote,
+                toggleActions: "restart pause resume reverse",
+                start: "top 50%",
+            },
+            duration: 0.6, 
+            ease: "circ.out", 
+            y: 80, 
+            stagger: 0.02,
+        });
     });
-
-    // Set up the anim
-    quote.anim = gsap.from(quote.split.chars, {
-      scrollTrigger: {
-        trigger: quote,
-        toggleActions: "restart pause resume reverse",
-        start: "top 50%",
-      },
-      duration: 0.6, 
-      ease: "circ.out", 
-      y: 80, 
-      stagger: 0.02,
-    });
-  });
 }
 
-ScrollTrigger.addEventListener("refresh", setupSplits);
-setupSplits();
+ScrollTrigger.addEventListener("refresh", splitText1);
+splitText1();
+// Fin effect1
