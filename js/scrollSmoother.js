dbf7c418e429edf34452aa430760306bf461dd7e
edzayaa <138071948+edzayaa@users.noreply.github.com>
2023-12-14 12:51:15 -0400

    update
    Create Init ScrollSmoother and docs

---
+++ js/scrollSmoother.js
@@ -0,0 +1,10 @@
+/*------------------------------
+Init ScrollSmoother
+------------------------------*/
+const smoother = ScrollSmoother.create({
+  smooth: 2,
+  //speed: 2,
+  effects: true,
+  normalizeScroll: true,
+  smoothTouch: 0.1,
+});
