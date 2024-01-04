9e1a4809ab24055549d13238256031bf54098921
edzayaa <138071948+edzayaa@users.noreply.github.com>
2024-01-03 20:09:06 -0400

    update
    update

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,7 +1,16 @@
 /*------------------------------
 Init ScrollTrigger // Inicio de ScrollTrigger
 ------------------------------*/
-gsap.to(".box", {
-    scrollTrigger: ".box", // start animation when ".box" enters the viewport
-    x: 500,
-  });
+const tl = gsap.timeline({
+scrollTrigger: {
+    trigger: ".trigger",
+    start: "top top",
+    end: "+=1000",
+    scrub: 1,
+    pin: true,
+    markers: true
+}
+});
+tl.to(".box", {yPercent: 350, duration: 1})
+tl.to(".box", {rotation: 360, duration: 3})
+tl.to(".box", {xPercent: 350, duration: 1})
